4bf74349143686396180b5594ab86e7cbbf78c1a
Greg Renfro <remph819@gmail.com>
2019-07-11 19:09:29 -0700

    added student parameters

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,9 +25,57 @@ class Instructor extends Person {
 
 class Student extends Person {
     constructor(child) {
+        super(child)
         this.previousBackground = child.previousBackground;
         this.className = child.className;
         this.favSubjects = child.favSubjects;
     }
-
+    listsSubjects() {
+        return
+    }
+    PRAssignment() {
+        return 
+    }
 }
+
+class ProjectManagers extends Instructor {
+    constructor(props) {
+        super(props);
+        this.gradClassName= props.gradClassName;
+        this.favInstructor = props.favInstructor;
+    }
+    standUp() {
+        return `${name} announces to ${channel}, @${channel} standy times.`
+    }
+    debugsCode() {
+        return `${name} debugs ${student.name}'/s code on ${subject}.`
+    }
+}
+
+
+const fred = new Student({
+    previousBackground: 'scientist',
+    className: 'WEBPT8',
+    favSubjects: 'HTML'
+});
+
+const evan = new Student({
+    previousBackground: 'singer',
+    className: 'WEBPT7',
+    favSubjects: 'CSS'
+   });
+
+const lori = new Student({
+    previousBackground: 'best friend',
+    className: 'WEBPT8',
+    favSubjects: 'HTML'
+});
+
+const joe = new Instructor({
+    specialty: 'redux',
+    favLanguage: 'Python',
+    catchPhrase: 'Do not forget the homies'
+});
+
+
+
